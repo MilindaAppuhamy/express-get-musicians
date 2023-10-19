f6df8a9858d3cf41ce6690c0be8d7e0a87199165
MilindaAppuhamy <milindaappuhamy.vodafone@gmail.com>
2023-10-19 14:58:37 +0100

    completed part 1.

--- index.test.js
+++ index.test.js
@@ -1,22 +1,24 @@
 // install dependencies
-const { execSync } = require('child_process');
-execSync('npm install');
-execSync('npm run seed');
+const { execSync } = require("child_process");
+execSync("npm install");
+execSync("npm run seed");
 
-const request = require("supertest")
-const { db } = require('./db/connection');
-const { Musician } = require('./models/index')
-const app = require('./src/app');
+const request = require("supertest");
+const { db } = require("./db/connection");
+const { Musician } = require("./models/index");
+const app = require("./src/app");
 const seedMusician = require("./seedData");
 
+describe("./musicians endpoint", () => {
+  it("should return all the musicians", async () => {
+    const res = await request(app).get("/musicians");
+    expect(res.statusCode).toBe(200);
+    expect(res.body.length).toEqual(3);
+  });
 
-describe('./musicians endpoint', () => {
-    // Write your tests here
-    
-    
-
-
-
-
-    
-})
+  it("should return a specific musician when id is passed", async () => {
+    const res = await request(app).get("/musicians/2");
+    expect(res.statusCode).toBe(200);
+    expect(res.body.id).toEqual(2);
+  });
+});
